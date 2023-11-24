4474b8e3698d3be38d034e00208259c973b9a714
jbdura <bidurajohnson@gmail.com>
2023-11-24 12:40:41 +0300

    Added views and urls of messaging app

--- api/messaging/views.py
+++ api/messaging/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Message, Response
+from .serializers import MessageSerializer, ResponseSerializer
+
 
 # Create your views here.
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+
+class ResponseViewSet(viewsets.ModelViewSet):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
