7d58567e6c272ff09d6d63bc8f2fc59b0e22a39a
jbdura <bidurajohnson@gmail.com>
2023-11-24 11:24:38 +0300

    Edited the messaging/models

--- api/messaging/models.py
+++ api/messaging/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from accounts.models import Agent, Customer
+
 
 # Create your models here.
+
+class Message(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_resolved = models.BooleanField(default=False)
+
+
+class Response(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
