a23ecfbd56ff1592dbbe79c9e738121dc1d036e1
jbdura <bidurajohnson@gmail.com>
2023-11-25 15:18:12 +0300

    Edited settings and messaging/urls

--- api/messaging/urls.py
+++ api/messaging/urls.py
@@ -1,20 +1,61 @@
-from rest_framework import routers
-from .views import MessageViewSet, ResponseViewSet
+# messaging/urls.py
+from django.urls import path
+from .views import CustomerMessagesView, AnswerMessageView, MessageViewSet, ResponseViewSet
+from rest_framework.routers import DefaultRouter
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'messages', MessageViewSet)
 router.register(r'responses', ResponseViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('customers/<int:customer_pk>/messages/', CustomerMessagesView.as_view(), name='customer-messages'),
+    path('customers/<int:customer_pk>/answer-message/', AnswerMessageView.as_view(), name='answer-message'),
+]
+
+urlpatterns += router.urls
+
+
+########################################################################################################################
+
 
 # # messaging/urls.py
-# from rest_framework import routers
-# from .views import MessageViewSet, ResponseViewSet
+# from django.urls import path
+# from .views import CustomerMessagesView, AnswerMessageView, MessageViewSet, ResponseViewSet
+# from rest_framework.routers import DefaultRouter
 #
-# router = routers.DefaultRouter()
+# router = DefaultRouter()
 # router.register(r'messages', MessageViewSet)
 # router.register(r'responses', ResponseViewSet)
 #
 # urlpatterns = [
-#     path('api/', include(router.urls)),
+#     path('customers/<int:customer_pk>/messages/', CustomerMessagesView.as_view(), name='customer-messages'),
+#     path('customers/<int:customer_pk>/answer-message/', AnswerMessageView.as_view(), name='answer-message'),
 # ]
+#
+# urlpatterns += router.urls
+#
+#######################################################################################################################
+#
+# # from rest_framework import routers
+# # from .views import MessageViewSet, ResponseViewSet
+# #
+# # router = routers.DefaultRouter()
+# # router.register(r'messages', MessageViewSet)
+# # router.register(r'responses', ResponseViewSet)
+# #
+# # urlpatterns = router.urls
+#
+#
+#
+#######################################################################################################################
+# # # messaging/urls.py
+# # from rest_framework import routers
+# # from .views import MessageViewSet, ResponseViewSet
+# #
+# # router = routers.DefaultRouter()
+# # router.register(r'messages', MessageViewSet)
+# # router.register(r'responses', ResponseViewSet)
+# #
+# # urlpatterns = [
+# #     path('api/', include(router.urls)),
+# # ]
